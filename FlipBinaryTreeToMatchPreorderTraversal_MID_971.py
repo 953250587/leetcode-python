438bea26ee96898c416d9585b53e0e6a03419ccc
pjq <953250587@qq.com>
2019-03-25 23:05:48 +0800

    leetcode 971, add another way of writing this code .

--- FlipBinaryTreeToMatchPreorderTraversal_MID_971.py
+++ FlipBinaryTreeToMatchPreorderTraversal_MID_971.py
@@ -84,6 +84,29 @@ class Solution(object):
         else:
             return [-1]
 
+    def flipMatchVoyage_1(self, root, voyage):
+        """
+        :type root: TreeNode
+        :type voyage: List[int]
+        :rtype: List[int]
+        44 ms
+        12 MB
+        """
+
+        res = []
+        self.i = 0
+
+        def dfs(root):
+            if not root: return True
+            if root.val != voyage[self.i]: return False
+            self.i += 1
+            if root.left and root.left.val != voyage[self.i]:
+                res.append(root.val)
+                root.left, root.right = root.right, root.left
+            return dfs(root.left) and dfs(root.right)
+
+        return res if dfs(root) else [-1]
+
 
 if __name__ == '__main__':
     root = TreeNode(1)
